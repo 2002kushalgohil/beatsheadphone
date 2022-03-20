8ccf43f08732356f1f751b4823a0536e27ce3583
kushal gohil <2002kushalgohil@gmail.com>
2022-03-20 10:14:02 +0530

    Added Meat tag and compressed images

--- components/Layout.js
+++ components/Layout.js
@@ -3,6 +3,7 @@ import NavBar from "./Pages/NavBar";
 import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import Head from "next/head";
 export default function Layout({ children }) {
   useEffect(() => {
     AOS.init({
@@ -12,6 +13,10 @@ export default function Layout({ children }) {
   }, []);
   return (
     <div className="px-5 sm:px-10 md:px-14 text-gray-600 bg-white">
+      <Head>
+        <title>Beats Headphone</title>
+        <meta name="description" content="Beats Headphone" />
+      </Head>
       <div>
         <NavBar />
         {children}
